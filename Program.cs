fad60af34263e61a6edb5c926693715e02f486da
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-03-29 16:57:30 +0530

    [Chandra]

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace FileIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //FileDemo.FileExists();
+            //FileDemo.ReadAllLines();
+            //FileDemo.ReadAllText();
+            //FileDemo.FileCopy();
+            //FileDemo.DeleteFile();
+            //FileDemo.WriteUsingStreamWriter();
+           // Console.ReadKey();
+
+            LearnFileIO.FileExisted();
+            LearnFileIO.ReadFileData();
+            LearnFileIO.CopyFileData();
+            LearnFileIO.WriteFileData();
+            LearnFileIO.ReadFileDatafromStream();
+        }
+    }
+}
